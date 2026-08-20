5b477586de4a7ef2d93fe50ac7cebf0c0f0df36b
13Dav-arc <akinsdavid05@gmail.com>
2026-08-20 18:41:12 +0100

    ci: add github pages automated deployment workflow

--- apps/studio/next.config.mjs
+++ apps/studio/next.config.mjs
@@ -1,10 +1,16 @@
+const isProd = process.env.NODE_ENV === 'production';
+const isCI = process.env.GITHUB_ACTIONS === 'true';
+const repoName = 'fix11y';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@fix11y/core'],
   output: 'export',
+  basePath: isCI ? `/${repoName}` : '',
+  assetPrefix: isCI ? `/${repoName}/` : '',
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
